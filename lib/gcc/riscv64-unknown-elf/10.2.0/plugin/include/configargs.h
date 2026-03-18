bf6449f267d1af281778ecc65453e7e1deb66bba
chenwanqi99 <chenwanqi99@163.com>
2026-03-18 21:42:21 +0800

    init

---
+++ lib/gcc/riscv64-unknown-elf/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/cluster/home/guoym/xuantie-gnu-toolchain/build/../riscv-gcc/configure --target=riscv64-unknown-elf CXXFLAGS='-g -O2 ' --disable-libgcctf --prefix=/cluster/home/guoym/xuantie-gnu-toolchain/opt/riscv --disable-shared --enable-threads=posix --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/cluster/home/guoym/xuantie-gnu-toolchain/opt/riscv/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=../../riscv-gcc --disable-multilib --with-abi=lp64d --with-arch=rv64imafdc 'CFLAGS_FOR_TARGET=-Os   -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os   -mcmodel=medlow'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc" } };
